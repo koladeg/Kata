890337e076ee4f9fe9d577691968e0c1e00497cb
koladeg <tayokay@yahoo.com>
2020-09-24 19:04:53 +0100

    add find shortest word in array kata

--- Mumbling.js
+++ Mumbling.js
@@ -1,10 +1,26 @@
 // Examples:
 // accum("abcd") -> "A-Bb-Ccc-Dddd"
 // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// function willFit(present, box){
+//   let fst = present.reduce(function(a, arr) {return a + arr}, 0)
+//   let snd = box.reduce(function(a, arr){return a + arr}, 0)
+//    return fst < snd
+// }
 
 function accum(s) {
     return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
   }
 
 console.log(accum("HbideVbxncC"))
+
+// Kata to find shortest word
+function findShort(s){
+  let fst = s.split(' ');   
+  let w = fst[0];
+  for (i of fst) {if (i.length < w.length){      w = i } }
+    return w.length
+ }
+
+function findShort(s){
+  return Math.min(...s.split(" ").map (s => s.length));
+}
