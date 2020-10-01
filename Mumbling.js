6d0258d061af76c8d35970c0b75043c7ecee58c8
koladeg <tayokay@yahoo.com>
2020-10-01 13:25:25 +0100

    edit katas

--- Mumbling.js
+++ Mumbling.js
@@ -45,3 +45,26 @@ function harmlessRansomNote(noteText, magazineText) {
   return noteIsPossible;
 }
 harmlessRansomNote('this is a secret note for you from a secret admirer', 'puerto rico is a place of great wonder and excitement it has many secret waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited');
+
+
+// Palindrome algorithm
+
+function isPalindrome(string) {
+  string = string.toLowerCase();
+  var charactersArr = string.split('');
+  var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var lettersArr = [];
+  charactersArr.forEach(char => {
+    if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+  });
+  return lettersArr.join('') === lettersArr.reverse().join('');
+}
+
+function palindrome(note) {
+  const clean = (str) => str.toLowerCase().replace(/[\W_]/g, '')
+  const cleannt = clean(note);
+  return cleannt.split('').every((c, i) => c === cleannt[cleannt.length -1 -i])
+}
+
+isPalindrome("Madam, I'm Adam");
+
