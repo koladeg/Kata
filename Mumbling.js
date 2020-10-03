84397ed06d2197c609333c867fbc265ddafb97f2
koladeg <tayokay@yahoo.com>
2020-10-03 11:32:15 +0100

    add caeser cipher

--- Mumbling.js
+++ Mumbling.js
@@ -69,27 +69,30 @@ function palindrome(note) {
 isPalindrome("Madam, I'm Adam");
 
 
-// Ceaser cipher
-function caesarCipher(str,num) {
+// Ceaser cipher algorithm :- finds letter in position num from letters in note 
+function ceaserCipher(note, num) {
   num = num % 26;
-  var lowerCaseString = str.toLowerCase();
-  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  var newString = '';
-  for (var i = 0; i < lowerCaseString.length; i++) {
-    var currentLetter = lowerCaseString[i];
-    if (currentLetter === ' ') {
-      newString += currentLetter;
+  const clean = (str) => str.toLowerCase()
+  let alpha = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  let str = ''
+  const cleannt = clean(note);
+  for ( i = 0; i < cleannt.length; i++){
+    let currentletter = cleannt[i]
+    if (currentletter == ' '){
+      str += currentletter;
       continue;
     }
-    var currentIndex = alphabet.indexOf(currentLetter);
-    var newIndex = currentIndex + num;
+    let currentIndex = alpha.indexOf(currentletter)
+    let newIndex = currentIndex + num;
     if (newIndex > 25) newIndex = newIndex - 26;
-    if (newIndex < 0) newIndex = 26 + newIndex;
-    if (str[i] === str[i].toUpperCase()) {
-      newString += alphabet[newIndex].toUpperCase();
+    if(newIndex < 0) newIndex = newIndex + 26;
+    if(note[i] === note[i].toUpperCase()){ 
+      str += alpha[newIndex].toUpperCase();
     }
-    else newString += alphabet[newIndex];
-  };
-  return newString;
+    else str += alpha[newIndex];
+    
+  }
+  return str;
 }
-caesarCipher('Zoo Keeper', 2);
+ceaserCipher("Big Car", -16);
+ceaserCipher("Javascript", -900);
