9b2879eaf4416105d9b9d38674cabe321d26a016
koladeg <tayokay@yahoo.com>
2020-10-06 09:08:40 +0100

    add reverse word algorithm

--- Mumbling.js
+++ Mumbling.js
@@ -106,3 +106,17 @@ function reverseWords (string) {
   return arr
 }
 reverseWords("the Big Car");
+
+function reverseWords(string) {
+  var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
+  wordsArr.forEach(word => {
+    var reversedWord = '';
+    for (var i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i];
+    };
+    reversedWordsArr.push(reversedWord);
+  });
+  return reversedWordsArr.join(' ');
+}
+reverseWords('Coding JavaScript');
